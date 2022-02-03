764d5de15b24554f6cfc74801483b0653a1c3bc0
ianarizpe <IanArizpe@yahoo.com>
2022-02-03 14:25:30 -0800

    test sim test

--- TestSim.py
+++ TestSim.py
@@ -137,7 +137,7 @@ def main():
     s.bootAll();
     s.addChannel(s.COMMAND_CHANNEL);
     s.addChannel(s.GENERAL_CHANNEL);
-    #s.addChannel(s.NEIGHBOR_CHANNEL);
+    s.addChannel(s.NEIGHBOR_CHANNEL);
     #s.addChannel(s.FLOODING_CHANNEL);
     #s.neighborDMP(7);
 
