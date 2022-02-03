9b9cc7667449c1c0ce8d109b18f21af3fcb77d56
ianarizpe <IanArizpe@yahoo.com>
2022-02-03 14:20:07 -0800

    crossingoutextra channels

--- TestSim.py
+++ TestSim.py
@@ -137,10 +137,9 @@ def main():
     s.bootAll();
     s.addChannel(s.COMMAND_CHANNEL);
     s.addChannel(s.GENERAL_CHANNEL);
-    s.addChannel(s.NEIGHBOR_CHANNEL);
-    s.addChannel(s.FLOODING_CHANNEL);
-
-    s.neighborDMP(7);
+    #s.addChannel(s.NEIGHBOR_CHANNEL);
+    #s.addChannel(s.FLOODING_CHANNEL);
+    #s.neighborDMP(7);
 
     s.runTime(20);
     s.ping(1, 2, "Hello, World");
